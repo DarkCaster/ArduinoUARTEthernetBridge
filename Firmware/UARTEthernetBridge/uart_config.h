ea79524fc77910032a074f00cd743083f2e82c0c
DarkCaster <dark.caster@outlook.com>
2021-05-08 22:24:41 +0300

    Firmware: added UARTConfig helper struct

---
+++ Firmware/UARTEthernetBridge/uart_config.h
@@ -0,0 +1,17 @@
+#ifndef UART_CONFIG_H
+#define UART_CONFIG_H
+
+#include <Arduino.h>
+
+#define CFG_RESET_REQUESTED(cfg) ((cfg.flags&0x1)!=0)
+#define CFG_FAKE_UART_MODE(cfg) ((cfg.flags&0x2)!=0)
+
+struct UARTConfig
+{
+    unsigned long speed;
+    uint8_t mode;
+    uint8_t flags;
+    uint8_t collectIntMS;
+};
+
+#endif // UART_CONFIG_H

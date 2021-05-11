7ff430a8395053e9bc395dfe17aa655afeb7c244
DarkCaster <dark.caster@outlook.com>
2021-05-12 01:36:23 +0300

    Client: IMessage updated

--- Client/Src/IMessage.h
+++ Client/Src/IMessage.h
@@ -1,11 +1,12 @@
 #ifndef IMESSAGE_H
 #define IMESSAGE_H
 
+#include "RemoteConfig.h"
+
 enum MsgType
 {
     MSG_SHUTDOWN,
-    MSG_STARTUP_READY,
-    MSG_STARTUP_CONTINUE,
+    MSG_NEW_CLIENT,
 };
 
 class IMessage
@@ -24,16 +25,13 @@ class IShutdownMessage : public IMessage
         const int ec;
 };
 
-class IStartupReadyMessage : public IMessage
+class INewClientMessage : public IMessage
 {
     protected:
-        IStartupReadyMessage():IMessage(MSG_STARTUP_READY){}
-};
-
-class IStartupContinueMessage : public IMessage
-{
-    protected:
-        IStartupContinueMessage():IMessage(MSG_STARTUP_CONTINUE){}
+        INewClientMessage(int _fd, const RemoteConfig &_remoteConfig):IMessage(MSG_NEW_CLIENT),fd(_fd),remoteConfig(&_remoteConfig){}
+    public:
+        const int fd;
+        const RemoteConfig * const remoteConfig;
 };
 
 #endif // IMESSAGE_H

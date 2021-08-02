0c7eac2fe6b27de9568783c78208209e84688d3c
DarkCaster <dark.caster@outlook.com>
2021-08-03 00:48:23 +0300

    Client: main updated

--- Client/Src/Main.cpp
+++ Client/Src/Main.cpp
@@ -31,7 +31,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-// example command line: -nd 1 -ra ENC28J65E366.lan -rp1 50000 -rp2 50001 -rp3 50002 -lp1 40000 -lp2 40001 -lp3 /tmp/ttyETH3 -ps1 250000 -ps2 250000 -ps3 115200 -pm1 255 -pm2 255 -pm3 6 -rst1 1 -rst2 1 -rst3 1
+// example command lines:
+// Arduino Pro Mini (atmega328p): -ra ENC28J65E366.lan -tp 50000 -pc 1 -lp1 65001 -ps1 250000 -pm1 255 -rst1 1 -rbs 10
+
 void usage(const std::string &self)
 {
     std::cerr<<"Usage: "<<self<<" [parameters]"<<std::endl;
